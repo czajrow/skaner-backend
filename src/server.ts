8d000f6342e4c73de2b0e77f7c63e8f3c32c2e7e
Adam Czajka <czajka.adam147@gmail.com>
2020-12-19 01:49:20 +0100

    Imporve ordering of db actions

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express = require("express");
 import { PROBE } from "./classes/probe";
 import { Database } from "./utils/database";
 import { from } from "rxjs";
+import { switchMap } from "rxjs/operators";
 
 const app = express();
 const database = new Database();
@@ -16,10 +17,13 @@ app.get('/', (req, res) => {
     // response: { value: string }
     res.status(200);
     const db = database.getDb();
-    from(db.collection('some_collection').insertOne({test: true, message: 'this is test'})).subscribe();
-    from(db.collection('some_collection').find().toArray()).subscribe(response => {
-        res.send({ total: response.length, messages: response.map(a => a.message) });
-    })
+    from(db.collection('some_collection').insertOne({ test: true, message: 'this is test' }))
+        .pipe(
+            switchMap(response => from(db.collection('some_collection').find().toArray())),
+        )
+        .subscribe(response => {
+            res.send({ total: response.length, messages: response.map(a => a.message) });
+        });
 });
 
 app.get('/api/probe', (req, res) => {
